09e3cbf2f645d92c1a515961af10e9134b2bfdc7
Rishi Rusia <rishirusia1@gmail.com>
2025-06-20 12:41:39 +0530

    Updated Server Listen

--- intervue-backend/index.js
+++ intervue-backend/index.js
@@ -221,7 +221,6 @@ app.get('/{*any}', (req, res) => {
   res.sendFile(path.join(distPath, 'index.html'));
 });
 
-
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
